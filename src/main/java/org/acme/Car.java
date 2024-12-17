60aa2d69546488e003c10a3bdc037bf5c0712fb0
Jackinatox <58728356+Jackinatox@users.noreply.github.com>
2024-12-17 16:21:10 +0100

    Getter/Setter for Car

--- src/main/java/org/acme/Car.java
+++ src/main/java/org/acme/Car.java
@@ -15,6 +15,62 @@ public class Car extends PanacheEntity {
     @ManyToOne
     private AppUser user;
 
+    public int getSeats() {
+        return seats;
+    }
+
+    public void setSeats(int seats) {
+        this.seats = seats;
+    }
+
+    public String getPlate() {
+        return plate;
+    }
+
+    public void setPlate(String plate) {
+        this.plate = plate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
+
+    public float getKilometerPrice() {
+        return kilometerPrice;
+    }
+
+    public void setKilometerPrice(float kilometerPrice) {
+        this.kilometerPrice = kilometerPrice;
+    }
+
+    public float getBasePrice() {
+        return basePrice;
+    }
+
+    public void setBasePrice(float basePrice) {
+        this.basePrice = basePrice;
+    }
+
+    public AppUser getUser() {
+        return user;
+    }
+
+    public void setUser(AppUser user) {
+        this.user = user;
+    }
+
     public Car() {
     }
 
